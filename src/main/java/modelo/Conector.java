8a90f365f24af584bcd236b4404e96ae40842f4a
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-03-15 14:28:28 +0100

    first commit

---
+++ src/main/java/modelo/Conector.java
@@ -0,0 +1,44 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+
+	protected Connection conexion;
+
+	public void conectar() {
+
+		conexion = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost/hotel", "root", "");
+			System.out.println("funciona");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("no funciona");
+
+		}
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+
+	public void cerrar() {
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/modelo/ModeloUsuario.java
@@ -0,0 +1,62 @@
+package modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import clases.Usuario;
+
+public class ModeloUsuario extends Conector{
+	
+	public Usuario getUsuario(int id) {
+		
+		String st = "SELECT * FROM usuaios WHERE id = ?";
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement(st);
+			pst.setInt(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			rs.next();
+			
+			Usuario usuario = new Usuario();
+			usuario.setId(rs.getInt("id"));
+			usuario.setNombre(rs.getString("nombre"));
+			usuario.setApellido(rs.getString("apellido"));
+			usuario.setDni(rs.getString("dni"));
+			usuario.setEdad(rs.getInt("edad"));
+			
+			return usuario;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	 public ArrayList<Usuario> getUsuarios() {
+		 
+		 String st = "SELECT * FROM usuarios";
+		 
+		 try {
+			PreparedStatement pst = super.conexion.prepareStatement(st);
+			
+			ResultSet rs = pst.executeQuery();
+			ArrayList<Usuario> usuarios = new ArrayList<>();
+			
+			while (rs.next()) {
+				usuarios.add(getUsuario(rs.getInt("id")));
+			}
+			
+			return usuarios;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		return null;
+	}
+
+}
